03814ca7265cd216a5e7fd738d9c35bcdbbd2235
kellynauert <kellynauert@gmail.com>
2021-02-11 20:50:22 -0500

    user controller and middleware

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
-module.exports = function(req, res, next) {
-    res.header('access-control-allow-origin', '*');
-=======
 module.exports = function (req, res, next) {
-    res.header('access-control-allow-origin', "*");
->>>>>>> 1b903a0fa3ca629bd6c1721c9c36cf0e0c577ffd
-    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
-    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('access-control-allow-origin', '*');
+	res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+	res.header(
+		'access-control-allow-headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
 
-    next();
+	next();
 };
