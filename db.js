a056dea2a0daafa71f25998453e5befc94c6949f
Kenneth Griggs <kenrgriggs@gmail.com>
2021-05-09 17:19:07 -0400

    thirteenth attempt at heroku deploy

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true
+    ssl: true,
+    rejectUnauthorized: false
   }
 });
 
